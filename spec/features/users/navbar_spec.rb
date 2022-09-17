7934d1fff3f38f4107b5271ddaa6698e4fe5c7f5
Sunny Moore <moore.sunny@gmail.com>
2022-09-17 16:44:29 -0700

    Updated button test in navbar_spec to a link

--- spec/features/users/navbar_spec.rb
+++ spec/features/users/navbar_spec.rb
@@ -1,16 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Nav Bar' do
-  it 'has a button on each page to Home' do
+  it 'has a link on each page to Home' do
     visit root_path
 
-    expect(page).to have_button('Home')
+    expect(page).to have_link('Home')
     click_on 'Home'
     expect(current_path).to eq(root_path)
-  end
-  it 'has a button on each page to Discover Venues' do
-    visit root_path
-
-    expect(page).to have_button('Discover Venues')
   end
 end
