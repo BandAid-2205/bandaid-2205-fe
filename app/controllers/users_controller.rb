7628f5870d0e2772fd73056c0a023a88c9f7a903
Sunny Moore <moore.sunny@gmail.com>
2022-09-18 14:49:48 -0700

    Set up Roles Controller. Changed User controller create method to redirect to roles rather than dashboard.  Added artist/venue controllers and views.  Added tests for roles page and that the links go where they should.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,13 @@ class UsersController < ApplicationController
     auth_hash = request.env['omniauth.auth']
     user = User.find_or_create_by(email: auth_hash[:info][:email], uid: auth_hash[:uid], token: auth_hash[:credentials][:token])
     session[:access_token] = auth_hash[:credentials][:token]
-    redirect_to '/dashboard'
+    session[:user_id] = user.id
+    # binding.pry
+    redirect_to '/roles'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :uid, :token)
   end
 end
