af06e8e5b02d872f999d79e0f06c8a897951d4ce
Sunny Moore <moore.sunny@gmail.com>
2022-09-18 10:46:44 -0700

    Removed Discover Events from the navbar, updated tests and scss files

--- spec/features/users/navbar_spec.rb
+++ spec/features/users/navbar_spec.rb
@@ -7,12 +7,10 @@ RSpec.describe 'Nav Bar' do
     expect(page).to have_link('Home')
     click_on 'Home'
     expect(current_path).to eq(root_path)
-  end
-  it 'has a link on each page to Discover Events' do
-    visit root_path
 
-    expect(page).to have_link('Discover Events')
-    click_on 'Discover Events'
-    expect(current_path).to eq(events_path)
+    visit events_path
+    expect(page).to have_link('Home')
+    click_on 'Home'
+    expect(current_path).to eq(root_path)
   end
 end
