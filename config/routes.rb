4146688aec5b589468d29a6d7fe141d5d45af20c
Sunny Moore <moore.sunny@gmail.com>
2022-09-17 17:22:10 -0700

    Added Discover Event link to navbar, and tested. Added EventsController. Added empty Events index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/', to: 'welcome#index'
+  get '/events', to: 'events#index'
   get '/dashboard', to: 'users#show'
 
   get '/auth/google_oauth2', as: :google_login
--- spec/features/users/navbar_spec.rb
+++ spec/features/users/navbar_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe 'Nav Bar' do
     click_on 'Home'
     expect(current_path).to eq(root_path)
   end
+  it 'has a link on each page to Discover Events' do
+    visit root_path
+
+    expect(page).to have_link('Discover Events')
+    click_on 'Discover Events'
+    expect(current_path).to eq(events_path)
+  end
 end
