a5a54ad8f673588c8f99d70ab025c9cfd097bf3b
Sunny Moore <moore.sunny@gmail.com>
2022-09-20 09:28:54 -0700

    Update artist poro to reflect backend model. Update test

--- spec/poros/artist_spec.rb
+++ spec/poros/artist_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Artist do
   it 'exists' do
-    data = {name: 'Jay-Z', city: 'Brooklyn, NY'}
+    data = {name: 'Jay-Z', location: 'Brooklyn, NY', genre: 'rap', image_path: '/assets/test/image.png'}
 
-    expect(Artist.new(data).name).to eq('Jay-Z')
-    expect(Artist.new(data).city).to eq('Brooklyn, NY')
+    artist = Artist.new(data)
+    expect(artist.name).to eq('Jay-Z')
+    expect(artist.location).to eq('Brooklyn, NY')
+    expect(artist.genre).to eq('rap')
+    expect(artist.image_path).to eq('/assets/test/image.png')
   end
 end
