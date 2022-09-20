be0b09d713f26f4e87db6c4d7e125286278d1bd4
Sunny Moore <moore.sunny@gmail.com>
2022-09-20 09:25:04 -0700

    Updated Artist Service with urls, and a method to import artist.  Updated tests for both artist_details and artist_import

--- spec/services/artist_service_spec.rb
+++ spec/services/artist_service_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'Artist Service' do
-  it 'when I call a endpoint' do
-    
+  it 'can retrieve an artist from database by user id', :vcr do
+    # input --> artist user_id
+    # output --> parsed json
+    parsed_json = ArtistService.artist_details(1000)
+
+    expect(parsed_json).to be_a Hash
+    expect(parsed_json[:data][:type]).to eq('artist')
+
+    artist = parsed_json[:data][:attributes]
+
+    expect(artist[:name]).to be_a String
+    expect(artist[:name]).to eq('Annie Lennox')
+  end
+  it 'can retrieve artist lastfm info and parse response', :vcr do
+    # input --> name string
+    # output --> parsed json
+    parsed_json = ArtistService.artist_import("Annie Lennox")
+
+    expect(parsed_json).to be_a Hash
+    expect(parsed_json[:data][:type]).to eq('artist_poro')
+
+    artist = parsed_json[:data][:attributes]
+
+    expect(artist[:name]).to be_a String
+    expect(artist[:name]).to eq('Annie Lennox')
   end
 end
