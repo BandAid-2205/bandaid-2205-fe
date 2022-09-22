b97a2893f0479c137b9de77b6590c813159907b5
Sunny Moore <moore.sunny@gmail.com>
2022-09-22 10:06:26 -0700

    Removed phone from artist poro, view and test. Updated artist facade test based on new seed info from backend

--- spec/features/artists/new_spec.rb
+++ spec/features/artists/new_spec.rb
@@ -7,19 +7,22 @@ RSpec.describe 'Artists#new' do
     expect(page).to have_content('Name:')
     expect(page).to have_content('Genre:')
     expect(page).to have_content('Location:')
-    expect(page).to have_content('Phone:')
     expect(page).to have_content('Tell me about yourself:')
   end
   it 'clicking import from last_fm will autofill the form from an api call' do
-    
+    # Can't test for bio due to the length of the returned string including double quotes
     visit "/artists/register"
+
     within('#artist-import') do
       fill_in :name, with: 'Annie Lennox'
       click_on "Import from Last.fm"
     end
     
-    expect(page).to have_field('Name', with: 'Annie Lennox')
-    expect(page).to have_field('Genre', with: 'female vocalists')
+    within('#artist-form') do
+      expect(page).to have_field('Name', with: 'Annie Lennox')
+      expect(page).to have_field('Genre', with: 'female vocalists') 
+    end
+    
 
     
   end
