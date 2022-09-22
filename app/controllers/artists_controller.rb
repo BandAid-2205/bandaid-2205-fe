e63fbc1fff49a288bcd980cb7632088dfe994977
Sunny Moore <moore.sunny@gmail.com>
2022-09-22 10:00:19 -0700

    Minor linting changes

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,21 +1,22 @@
 class ArtistsController < ApplicationController
-  def index
-    
-  end
+  def index; end
 
   def new
-    data= { name: '', location: '', genre: '', bio: '' }
+    data = { name: '', location: '', genre: '', bio: '' }
     @imported = Artist.new(data)
   end
 
   def import
-    if params[:name] == nil
-      flash[:error] = "Name cannot be blank"
+    if params[:name].nil?
+      flash[:error] = 'Name cannot be blank'
       redirect_to '/artists/register'
     else
-      # binding.pry
       @imported = ArtistFacade.artist_import(params[:name])
       render action: :new
     end
+  end
+
+  def create
+    
   end
 end
