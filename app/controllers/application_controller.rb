f740fe3bedf64aa13051f5e53a365b18a9517ace
Justin Ramirez <jusrez528@gmail.com>
2022-09-23 00:49:49 -0400

    Add migration back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def refresh_current_user
+    user_id = current_user.id
+    current_user = User.find(user_id)
   end
 end
