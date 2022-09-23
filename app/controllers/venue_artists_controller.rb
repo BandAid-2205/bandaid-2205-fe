998d914279e6eefaa9567be02531bd6871b5f917
Justin Ramirez <jusrez528@gmail.com>
2022-09-23 00:55:31 -0400

    Change venue artists controller

--- app/controllers/venue_artists_controller.rb
+++ app/controllers/venue_artists_controller.rb
@@ -1,7 +1,7 @@
 class VenueArtistsController < ApplicationController
-  def update
-    venue_artist = #code that works with facade/service to retrieve venue artist
-    if params[:booking_status] == 1
-      venue_artist
-  end
+  # def update
+  #   venue_artist = #code that works with facade/service to retrieve venue artist
+  #   if params[:booking_status] == 1
+  #     venue_artist
+  # end
 end
